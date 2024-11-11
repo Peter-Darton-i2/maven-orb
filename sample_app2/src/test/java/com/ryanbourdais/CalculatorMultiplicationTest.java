7ae7cc534b139fc6fc3c1433dfe9aef882066c4a
Peter Darton <peter.darton@i2group.com>
2024-11-11 18:48:20 +0000

    Tidy up sample_app code

---
+++ sample_app2/src/test/java/com/ryanbourdais/CalculatorMultiplicationTest.java
@@ -0,0 +1,26 @@
+package com.ryanbourdais;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test
+ */
+public class CalculatorMultiplicationTest 
+{
+    @Test
+    public void shouldEqualTwentyOne()
+    {
+        float correctAnswer = 21;
+        assertEquals(correctAnswer, Calculator.Multiplication(7, 3), 0.000001);
+    }
+
+    @Test
+    public void DivisionTest()
+    {
+        float correctAnswer = 0.41666f;
+        assertEquals(correctAnswer, Calculator.Division(5, 12), 0.00001);
+    }
+}
--- sample_app2/src/test/java/com/ryanbourdais/CalculatorTest.java
+++ sample_app2/src/test/java/com/ryanbourdais/CalculatorTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class CalculatorTest
 {
     /**
      * Rigorous Test :-)
@@ -31,19 +31,5 @@ public class AppTest
     {
         float correctAnswer = 3;
         assertEquals(correctAnswer, Calculator.Subtraction(5, 2), 0.000001);
-    }
-
-    @Test
-    public void shouldEqualTwentyOne()
-    {
-        float correctAnswer = 21;
-        assertEquals(correctAnswer, Calculator.Multiplication(7, 3), 0.000001);
-    }
-
-    @Test
-    public void DivisionTest()
-    {
-        float correctAnswer = 0.41666f;
-        assertEquals(correctAnswer, Calculator.Division(5, 12), 0.00001);
     }
 }
