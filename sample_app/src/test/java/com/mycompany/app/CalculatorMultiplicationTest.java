d63982547af6c14e092d19764b7f1b897c38e57c
Peter Darton <peter.darton@i2group.com>
2024-11-12 11:33:57 +0000

    Improve parallelism testability on sample_app

--- sample_app/src/test/java/com/mycompany/app/CalculatorMultiplicationTest.java
+++ sample_app/src/test/java/com/mycompany/app/CalculatorMultiplicationTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for calculator multiplication and division
+ * Unit test for calculator multiplication
  */
 public class CalculatorMultiplicationTest 
 {
@@ -15,12 +15,5 @@ public class CalculatorMultiplicationTest
     {
         float correctAnswer = 21;
         assertEquals(correctAnswer, Calculator.Multiplication(7, 3), 0.000001);
-    }
-
-    @Test
-    public void DivisionTest()
-    {
-        float correctAnswer = 0.41666f;
-        assertEquals(correctAnswer, Calculator.Division(5, 12), 0.00001);
     }
 }
--- sample_app/src/test/java/com/mycompany/app/CalculatorSubtractionTest.java
+++ sample_app/src/test/java/com/mycompany/app/CalculatorSubtractionTest.java
@@ -6,28 +6,19 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for calculator, excluding multiplication and division
+ * Unit test for calculator subtraction
  */
-public class CalculatorTest
+public class CalculatorSubtractionTest
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void twoMinusThree()
     {
-        assertTrue( true );
-    }
-
-    @Test
-    public void shouldEqualFive()
-    {
-        float correctAnswer = 5;
+        float correctAnswer = -1;
         assertEquals(correctAnswer, Calculator.Addition(2,3), 0.000001);
     }
 
     @Test
-    public void shouldEqualThree()
+    public void fiveMinusTwo()
     {
         float correctAnswer = 3;
         assertEquals(correctAnswer, Calculator.Subtraction(5, 2), 0.000001);
